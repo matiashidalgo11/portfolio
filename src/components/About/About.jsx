c16b11e2bceee3e540ad7cbb85e236f3861b378c
matiashidalgo11 <matiashidalgo18@gmail.com>
2022-01-11 21:00:54 -0300

    correccion texto

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -28,7 +28,7 @@ export default function About(){
 
                         <div className={classes.ContainerMe}>
                             <h3>Buenas! 🖐</h3>
-                            <p>Soy un programador horientado al FullStack, me encanta programar y ademas de eso practicar deporte. Soy bastante activo y predispuesto.</p>
+                            <p>Soy un programador orientado al FullStack, me encanta programar y ademas de eso practicar deporte. Soy una persona activa y predispuesta.</p>
                             <a href="https://drive.google.com/file/d/1fTn8IuaFL3rMVi3B2QApsoLVb2QalLOg/view?usp=sharing" rel="noreferrer" target="_blank" className={classes.BtnResume}>Descargar CV</a>
                         </div>
                     </div>
